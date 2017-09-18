07a3cd8baaf64f0ee6a176a561cb0615e2faad4b
Robert Jones <robert.g.jones@gmail.com>
2017-09-18 11:06:56 +0100

    Show that clone is a deep clone
    Achieve this by testing for object equality and for the stringigy
    equality.

--- spec/deepCloneSpec.js
+++ spec/deepCloneSpec.js
@@ -1,9 +1,23 @@
 (function() {
-  // Test to show copies are the same object
+  // Test 1
   console.log("Checks that a copy is the same object");
   var json = { name: "Paddy", address: {town: "Lerum", country: "Sweden"}};
   var copy = json;
   assert.isEqual(json, copy);
-  console.log("Test-Passed");
+  console.log("Test 1 Passed");
+
+  // Test 2
+  console.log("Checks that the clone is different object");
+  var json = { name: "Paddy", address: {town: "Lerum", country: "Sweden"}};
+  var clone = deepClone(json);
+  assert.isNotEqual(json, clone);
+  console.log("Test 2 Passed");
+
+  // Test 3
+  console.log("Checks that the content of the clone is identical to original object");
+  var json = { name: "Paddy", address: {town: "Lerum", country: "Sweden"}};
+  var clone = deepClone(json);
+  assert.isEqual(JSON.stringify(json), JSON.stringify(clone));
+  console.log("Test 3 Passed");
 
 })();
---
+++ src/deepClone.js
@@ -0,0 +1,17 @@
+(function(exports) {
+
+  function deepClone(obj) {
+    var clone = {};
+    for (var key in obj) {
+       if (typeof obj[key] === "object") {
+           clone[key] = deepClone(obj[key]);
+       } else {
+           clone[key] = obj[key];
+       }
+     }
+     return clone;
+  }
+
+  exports.deepClone = deepClone;
+  
+})(this);
